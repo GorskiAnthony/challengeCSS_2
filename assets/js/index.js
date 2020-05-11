7fc210ceeb6f43366a2f7fe9baeedb354c37e174
Anthony Gorski <anthony.developpeurweb@gmail.com>
2020-05-12 00:12:26 +0200

    It's so difficult for me :sweat_smile:

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,30 @@
 
+/* init error to 0 */
+let error = 0;
+
+/* Function valideForm */
 function validateForm() {
     event.preventDefault();
 
-    const mailValue = document.getElementById("mail").value;
+    const mail = document.getElementById("mail");
+    const mailValue = mail.value;
+    
+    /** Controle Mail */
+    /** Just controle @ into my email */
+    const parseMail = mailValue.split("@");
 
-    if(mailValue !== "") {
-        console.log("OK")
-        console.log("Mail : ", mailValue);
+    /** if value is different to empty so mail valide */
+    if(mailValue !== "" && parseMail.length === 2 ) {
+        error = 0;
+        mail.classList.remove("error");
+        document.getElementById("error").innerHTML = "";
+        /** else if value egal to empty AND error egal 1 so add class ERROR to my input*/
+    } else if( (mailValue === "" || parseMail !== 2) && error === 1) {
+        mail.classList.add("error");
+        /** else add a text for explain error and add class error */
     } else {
         document.getElementById("error").innerHTML += "Please provide a valid email";
-        mailValue.classList.add("error");
+        mail.classList.add("error");
+        error = 1;
     }
-    
 }
